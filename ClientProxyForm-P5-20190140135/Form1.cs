9d19de5d82cea66c7980a53d2db63395873998f3
IlhamRobbS <IlhamRobbS@LAPTOP-AUHI2QMK>
2021-11-16 00:48:59 +0700

    Add project files.

---
+++ ClientProxyForm-P5-20190140135/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClientProxyForm_P5_20190140135
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
